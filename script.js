b0766a379363ae07ed6d8b4eaa892ef54da5dd33
kyara <angelakyara2021@gmail.com>
2025-09-09 11:08:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ if(nome === "admim"){
 }else if(nome === "moderador" && idade >=16){
     alert("Olá, Moderador! Acesso liberado para a àrea de moderação.")
 }else if(idade >=18){
-    alert('Olá,{nome}! Você tem acesso completo. Boas-vindas à plataforma! ')
+    alert(`Olá,${nome}! Você tem acesso completo. Boas-vindas à plataforma!  `)
 }else if(idade >=13 && idade <=17){
     alert("Acesso negado. Você pode participar do nosso programa Jovem Aprendiz!")
 }else{
     alert("Acesso negado. O conteúdo é exclusivo para maiores de 18 anos")
 }
+
 
